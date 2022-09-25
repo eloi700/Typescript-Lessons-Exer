1a5537325759efae363a738cf2b0c04bdb8a428d
Cielo <macielo.tablate@gmail.com>
2022-09-25 17:17:25 +0200

    add files

---
+++ helloworld/dist/index-2.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=index-2.js.map
--- helloworld/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function sayHi() {
-    console.log('Hi');
-}
-let speech = sayHi();
-console.log(speech);
-//# sourceMappingURL=index.js.map
